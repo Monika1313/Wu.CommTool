3454a18c9b1be8eb1a5b88353c52514ef8f236a3
Monika <961501261@qq.com>
2022-07-20 11:53:07 +0800

    添加项目文件。

---
+++ Wu.ComTool/ViewModels/DesignViewModels/MainWindowDesignViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Wu.ComTool.ViewModels.DesignViewModels
+{
+    public class MainWindowDesignViewModel : MainWindowViewModel
+    {
+        private static MainWindowDesignViewModel _Instance;
+        public static MainWindowDesignViewModel Instance => _Instance ??= new MainWindowDesignViewModel();
+        public MainWindowDesignViewModel()
+        {
+
+        }
+    }
+}
---
+++ Wu.ComTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+
+namespace Wu.ComTool.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get => _Title; set => SetProperty(ref _Title, value); }
+        private string _Title = "串口调试工具";
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
