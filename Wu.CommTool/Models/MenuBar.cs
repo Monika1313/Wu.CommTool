f1dcda7f082bf199cdd11a1a8228dcbf757bed79
weibin wu <961501261@qq.com>
2022-07-21 23:12:19 +0800

    W

---
+++ Wu.CommTool/Models/MenuBar.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wu.Comm.Models
+{
+    /// <summary>
+    /// 系统导航菜单
+    /// </summary>
+    public class MenuBar : BindableBase
+    {
+        private string? icon;
+        /// <summary>
+        /// 菜单图标
+        /// </summary>
+        public string? Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+
+        private string? title;
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string? Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string? nameSpace;
+        /// <summary>
+        /// 菜单命名空间
+        /// </summary>
+        public string? NameSpace
+        {
+            get { return nameSpace; }
+            set { nameSpace = value; }
+        }
+
+    }
+}
--- Wu.CommTool/ViewModels/MainWindowViewModel.cs
+++ Wu.CommTool/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.ObjectModel;
 using Wu.Comm.Common;
 using Wu.Comm.Extensions;
+using Wu.Comm.Models;
+using Wu.Comm.Views;
 
 namespace Wu.Comm.ViewModels
 {
@@ -17,6 +19,7 @@ namespace Wu.Comm.ViewModels
         public string Title { get => _Title; set => SetProperty(ref _Title, value); }
         private string _Title = "串口调试工具";
         private readonly IRegionManager regionManager;
+        private IRegionNavigationJournal journal;
 
         public MainWindowViewModel()
         {
@@ -25,8 +28,48 @@ namespace Wu.Comm.ViewModels
         public MainWindowViewModel(IRegionManager regionManager)
         {
             this.regionManager = regionManager;
+
+            CreateMenuBar();
             ExecuteCommand = new DelegateCommand<string>(Execute);
+            NavigateCommand = new DelegateCommand<MenuBar>(Navigate);
+
+            GoBackCommand = new DelegateCommand(() =>
+            {
+                if (journal != null && journal.CanGoBack)
+                    journal.GoBack();
+            });
+            GoForwarCommand = new DelegateCommand(() =>
+            {
+                if (journal != null && journal.CanGoForward)
+                    journal.GoForward();
+            });
         }
+
+        #region 属性
+        /// <summary>
+        /// 主菜单
+        /// </summary>
+        public ObservableCollection<MenuBar> MenuBars { get => _MenuBars; set => SetProperty(ref _MenuBars, value); }
+        private ObservableCollection<MenuBar> _MenuBars;
+        #endregion
+
+        #region 命令
+        /// <summary>
+        /// 导航命令
+        /// </summary>
+        public DelegateCommand<MenuBar> NavigateCommand { get; private set; }
+
+        /// <summary>
+        /// 导航返回
+        /// </summary>
+        public DelegateCommand GoBackCommand { get; private set; }
+
+        /// <summary>
+        /// 导航前进
+        /// </summary>
+        public DelegateCommand GoForwarCommand { get; private set; }
+
+        #endregion
 
         private void Execute(string obj)
         {
@@ -49,6 +92,28 @@ namespace Wu.Comm.ViewModels
         }
 
 
+        /// <summary>
+        /// 创建主菜单
+        /// </summary>
+        void CreateMenuBar()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
+            MenuBars.Add(new MenuBar() { Icon = "TransitConnectionVariant", Title = "Com", NameSpace = nameof(ComToolView) });
+        }
+
+        /// <summary>
+        /// 窗口导航
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Navigate(MenuBar obj)
+        {
+            if (obj == null || string.IsNullOrWhiteSpace(obj.NameSpace))
+                return;
+            regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(obj.NameSpace, back =>
+            {
+                journal = back.Context.NavigationService.Journal;
+            });
+        }
 
     }
 }
