5a3aed6efdfe52283961dbd758963df1d25bd189
Monika <961501261@qq.com>
2022-07-22 14:23:15 +0800

    w

--- Wu.CommTool/Models/Parity.cs
+++ Wu.CommTool/Models/Parity.cs
@@ -14,15 +14,15 @@ namespace Wu.CommTool.Models
     [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum Parity
     {
-        [Description("None 无校验")]
-        None = 0,
-        [Description("Odd 奇校验")]
-        Odd = 1,
-        [Description("Even 偶校验")]
-        Even = 2,
-        [Description("Mark 固定1")]
-        Mark = 3,
-        [Description("Space 固定0")]
+        [Description("None   无校验")]
+        None = 0,                      
+        [Description("Odd    奇校验")]
+        Odd = 1,                       
+        [Description("Even   偶校验")]
+        Even = 2,                      
+        [Description("Mark   固定1")]
+        Mark = 3,                      
+        [Description("Space  固定0")]
         Space = 4,
     }
 }
