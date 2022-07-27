b7312017a3dfe26a8ff1700a194842dd8668b821
weibin wu <961501261@qq.com>
2022-07-27 23:34:41 +0800

    W

--- Wu.CommTool/ViewModels/DialogViewModels/DialogDesignViewModels/ModbusRtuAutoSearchDeviceDesignViewModel.cs
+++ Wu.CommTool/ViewModels/DialogViewModels/DialogDesignViewModels/ModbusRtuAutoSearchDeviceDesignViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ModbusRtuAutoSearchDeviceDesignViewModel : ModbusRtuAutoSearchDeviceViewModel
     {
-        private static ModbusRtuAutoSearchDeviceDesignViewModel _Instance;
+        private static ModbusRtuAutoSearchDeviceDesignViewModel _Instance = new();
         public static ModbusRtuAutoSearchDeviceDesignViewModel Instance => _Instance ??= new();
         public ModbusRtuAutoSearchDeviceDesignViewModel()
         {
