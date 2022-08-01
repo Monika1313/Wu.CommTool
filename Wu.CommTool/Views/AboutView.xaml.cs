4f1a3c1c8d8b87ef6ee2e5de90018198db67aebc
weibin wu <961501261@qq.com>
2022-08-01 23:27:06 +0800

    W

--- Wu.CommTool/Views/AboutView.xaml.cs
+++ Wu.CommTool/Views/AboutView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,19 @@ namespace Wu.CommTool.Views
         public AboutView()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Hyperlink link = sender as Hyperlink;
+                System.Diagnostics.Process.Start("explorer.exe",link.NavigateUri.AbsoluteUri);
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
