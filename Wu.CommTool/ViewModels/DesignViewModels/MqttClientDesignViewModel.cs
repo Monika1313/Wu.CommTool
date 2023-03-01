b9bc393ebb7779d23e0d7e2f5aafbeea5ced465c
Monika <961501261@qq.com>
2023-03-01 13:59:25 +0800

    Update : 更新Mqtt客户端UI

--- Wu.CommTool/ViewModels/DesignViewModels/MqttClientDesignViewModel.cs
+++ Wu.CommTool/ViewModels/DesignViewModels/MqttClientDesignViewModel.cs
@@ -6,7 +6,7 @@
         public static MqttClientDesignViewModel Instance => _Instance ??= new();
         public MqttClientDesignViewModel()
         {
-            IsDrawersOpen.IsLeftDrawerOpen = false;
+            IsDrawersOpen.IsLeftDrawerOpen = true;
             IsDrawersOpen.IsRightDrawerOpen = false;
             MqttClientConfig.SubscribeTopics.Add("Topic1");
             MqttClientConfig.SubscribeTopics.Add("Topic2");
