3c9e723e818cf62bf850d8c1d741829356e148a6
Monika <961501261@qq.com>
2023-03-21 16:32:38 +0800

    W

---
+++ Wu.CommTool.Modules.About/Views/AboutView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Wu.CommTool.Modules.About.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutView
+    /// </summary>
+    public partial class AboutView : UserControl
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+    }
+}
