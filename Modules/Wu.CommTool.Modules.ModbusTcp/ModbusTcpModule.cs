55c197ecb5fa051d702f5f3f8c675bd2c078b188
Monika <961501261@qq.com>
2023-04-21 09:28:14 +0800

    挪了下位置

---
+++ Modules/Wu.CommTool.Modules.ModbusTcp/ModbusTcpModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Wu.CommTool.Modules.ModbusTcp.Views;
+
+namespace Wu.CommTool.Modules.ModbusTcp
+{
+    public class ModbusTcpModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
