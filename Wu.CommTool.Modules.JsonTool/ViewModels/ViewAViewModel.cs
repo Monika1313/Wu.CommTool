ba8352fbe1b5a06a6772bb5baae97f994dbdf4cb
Monika <961501261@qq.com>
2023-04-21 10:23:27 +0800

    Add : 添加模块

---
+++ Wu.CommTool.Modules.JsonTool/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wu.CommTool.Modules.JsonTool.ViewModels
+{
+    public class ViewAViewModel : BindableBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public ViewAViewModel()
+        {
+            Message = "View A from your Prism Module";
+        }
+    }
+}
