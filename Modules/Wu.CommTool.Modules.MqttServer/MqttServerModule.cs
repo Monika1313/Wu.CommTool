6b775dc52de018fcedffc4155511f069375c3378
Monika <961501261@qq.com>
2023-05-09 09:48:31 +0800

    Add : 新增模块MqttServer, 将MqttServer分离为模块,并使用Mqttnet4.0版本

---
+++ Modules/Wu.CommTool.Modules.MqttServer/MqttServerModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Wu.CommTool.Modules.MqttServer.Views;
+
+namespace Wu.CommTool.Modules.MqttServer
+{
+    public class MqttServerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
