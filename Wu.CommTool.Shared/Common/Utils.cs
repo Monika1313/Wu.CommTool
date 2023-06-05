3f73c8191791179ae26677c9c534cb9bc6e6e26d
weibin wu <961501261@qq.com>
2023-06-05 22:27:38 +0800

    添加一些通用16进制字符处理方法

--- Wu.CommTool.Shared/Common/Utils.cs
+++ Wu.CommTool.Shared/Common/Utils.cs
@@ -1,30 +1,67 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Wu.CommTool.Shared.Common
 {
     public static class Utils
     {
         /// <summary>
-        /// 判断是否为16进制字符串
+        /// 判断是否仅含16进制字符
         /// </summary>
-        /// <param name="hexString"></param>
+        /// <param name="str"></param>
         /// <returns></returns>
-        public static bool IsHexString(string hexString)
+        public static bool IsHexString(string str)
         {
-            //十六进制发送时，发送框数据进行十六进制数据正则校验
-            if (Regex.IsMatch(hexString, "^[0-9A-Fa-f]+$"))
-            {
-                //校验成功
+            string val = str.Replace(" ", "");
+            //验证字符串仅包含16进制字符 至少一个字符
+            if (Regex.IsMatch(val, "^[0-9A-Fa-f]+$"))
                 return true;
-            }
-            //校验失败
             return false;
         }
+
+        /// <summary>
+        /// 判断是否为16位(包含1-4位16进制字符)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool Is16BitHexString(string str)
+        {
+            string val = str.Replace(" ", "");
+            //验证字符串仅包含1-4个16进制字符
+            if (Regex.IsMatch(val, "^[0-9A-Fa-f]{1,4}$"))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 判断是否为包含32位(包含1-8位16进制字符)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool Is32BitHexString(string str)
+        {
+            string val = str.Replace(" ", "");
+            //验证字符串仅包含1-8个16进制字符
+            if (Regex.IsMatch(val, "^[0-9A-Fa-f]{1,8}$"))
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 判断是否为64位(包含1-8位16进制字符)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool Is64BitHexString(string str)
+        {
+            string val = str.Replace(" ", "");
+            //验证字符串仅包含1-16个16进制字符
+            if (Regex.IsMatch(val, "^[0-9A-Fa-f]{1,16}$"))
+                return true;
+            return false;
+        }
+
+
+
+
     }
 }
