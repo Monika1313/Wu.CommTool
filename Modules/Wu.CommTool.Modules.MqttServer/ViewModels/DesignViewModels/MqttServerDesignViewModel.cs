ed49de0653cef20a50a11d4e9ad78c5b90f35db4
weibin wu <961501261@qq.com>
2023-07-23 15:14:23 +0800

    W

---
+++ Modules/Wu.CommTool.Modules.MqttServer/ViewModels/DesignViewModels/MqttServerDesignViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Wu.CommTool.Modules.MqttServer.ViewModels.DesignViewModels
+{
+    public class MqttServerDesignViewModel : MqttServerViewModel
+    {
+        private static MqttServerDesignViewModel _Instance = new();
+        public static MqttServerDesignViewModel Instance => _Instance ??= new();
+        public MqttServerDesignViewModel()
+        {
+
+        }
+    }
+}
---
+++ Wu.CommTool.Core/Extensions/HcGrowlExtensions.cs
@@ -0,0 +1,47 @@
+﻿using HandyControl.Controls;
+using HandyControl.Data;
+
+namespace Wu.CommTool.Core.Extensions
+{
+    /// <summary>
+    /// handycontrol Growl扩展方法
+    /// </summary>
+    public static class HcGrowlExtensions
+    {
+        //默认的停留时间
+        public const int WaitTime = 2;
+
+        public static void Warning(string message, string token = "", int waitTime = WaitTime, bool showDateTime = false)
+        {
+            Growl.Warning(new GrowlInfo()
+            {
+                Message = message,
+                WaitTime = waitTime,
+                Token = token,
+                ShowDateTime = showDateTime
+            });
+        }
+
+        public static void Success(string message, string token = "", int waitTime = WaitTime, bool showDateTime = false)
+        {
+            Growl.Success(new GrowlInfo()
+            {
+                Message = message,
+                WaitTime = waitTime,
+                Token = token,
+                ShowDateTime = showDateTime
+            });
+        }
+
+        public static void Info(string message, string token = "", int waitTime = WaitTime, bool showDateTime = false)
+        {
+            Growl.Info(new GrowlInfo()
+            {
+                Message = message,
+                WaitTime = waitTime,
+                Token = token,
+                ShowDateTime = showDateTime
+            });
+        }
+    }
+}
