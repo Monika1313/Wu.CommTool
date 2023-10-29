c98648f488beb234baa35d41d951520025a3814c
weibin wu <961501261@qq.com>
2023-10-29 23:50:27 +0800

    W

---
+++ Modules/Wu.CommTool.Modules.ModbusRtu/ViewModels/DialogViewModels/DialogDesignViewModels/AnalyzeFrameDesignViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Wu.CommTool.Modules.ModbusRtu.ViewModels.DialogViewModels.DialogDesignViewModels
+{
+    public class AnalyzeFrameDesignViewModel : AnalyzeFrameViewModel
+    {
+        private static AnalyzeFrameDesignViewModel _Instance = new();
+        public static AnalyzeFrameDesignViewModel Instance => _Instance ??= new();
+        public AnalyzeFrameDesignViewModel()
+        {
+
+        }
+    }
+}
---
+++ Modules/Wu.CommTool.Modules.ModbusRtu/ViewModels/DialogViewModels/DialogDesignViewModels/EditFrameDesignViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Wu.CommTool.Modules.ModbusRtu.ViewModels.DialogViewModels.DialogDesignViewModels
+{
+    public class EditFrameDesignViewModel : EditFrameViewModel
+    {
+        private static EditFrameDesignViewModel _Instance = new();
+        public static EditFrameDesignViewModel Instance => _Instance ??= new();
+        public EditFrameDesignViewModel()
+        {
+
+        }
+    }
+}
