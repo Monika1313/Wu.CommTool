62449b7613f42215976ba5350b29c145f712d214
weibin wu <961501261@qq.com>
2024-02-20 22:39:48 +0800

    W

--- Modules/Wu.CommTool.Modules.ModbusTcp/ModbusTcpModule.cs
+++ Modules/Wu.CommTool.Modules.ModbusTcp/ModbusTcpModule.cs
@@ -1,23 +1,19 @@
-﻿using Prism.Ioc;
-using Prism.Modularity;
-using Prism.Regions;
+﻿using Prism.Modularity;
 using Wu.CommTool.Modules.ModbusTcp.ViewModels;
-using Wu.CommTool.Modules.ModbusTcp.Views;
 
-namespace Wu.CommTool.Modules.ModbusTcp
+namespace Wu.CommTool.Modules.ModbusTcp;
+
+public class ModbusTcpModule : IModule
 {
-    public class ModbusTcpModule : IModule
+    public void OnInitialized(IContainerProvider containerProvider)
     {
-        public void OnInitialized(IContainerProvider containerProvider)
-        {
 
-        }
+    }
 
-        public void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.RegisterForNavigation<ModbusTcpView, ModbusTcpViewModel>();   //ModbusTcp主界面 
-            containerRegistry.RegisterForNavigation<ModbusTcpCustomFrameView, ModbusTcpCustomFrameViewModel>();//ModbusTcp自定义帧主界面 
-            containerRegistry.RegisterForNavigation<ModbusTcpMasterView, ModbusTcpMasterViewModel>();//ModbusTcp自定义帧主界面 
-        }
+    public void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterForNavigation<ModbusTcpView, ModbusTcpViewModel>();   //ModbusTcp主界面 
+        containerRegistry.RegisterForNavigation<ModbusTcpCustomFrameView, ModbusTcpCustomFrameViewModel>();//ModbusTcp自定义帧主界面 
+        containerRegistry.RegisterForNavigation<ModbusTcpMasterView, ModbusTcpMasterViewModel>();//ModbusTcp自定义帧主界面 
     }
 }
