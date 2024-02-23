bba08808046f9613bd7d395dc58944588e94131a
Monika <961501261@qq.com>
2024-02-23 14:00:38 +0800

    W

--- Wu.CommTool.Shared/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿//using System.Windows.Markup;
-
-//[assembly:XmlnsDefinition("Wu.CommTool.Shared","Wu.CommTool.Shared")]
-////[assembly:XmlnsDefinition("Wu.CommTool.Shared","Wu.CommTool.Shared.Enums")]
-////[assembly:XmlnsDefinition("Wu.CommTool.Shared","Wu.CommTool.Shared.Enums.Mqtt")]
-////[assembly:XmlnsDefinition("Wu.CommTool.Shared","Wu.CommTool.Shared.Models")]
-//[assembly:XmlnsPrefix("Wu.CommTool.Shared","shared")]
