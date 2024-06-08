ca657ca0f397f971a0b76740f82c5ec966c83f0c
Dmitry <francuzov_dm@mail.ru>
2024-06-08 21:54:27 +0700

    Removed extra lines, fixed bug, removed unecessary null checks

--- Modules/Wu.CommTool.Modules.ModbusRtu/Models/ModbusRtuModel.cs
+++ Modules/Wu.CommTool.Modules.ModbusRtu/Models/ModbusRtuModel.cs
@@ -642,7 +642,7 @@ public class ModbusRtuModel : BindableBase
         try
         {
             //若串口未开启则返回
-            if (!SerialPort.IsOpen)
+            if (SerialPort != null && !SerialPort.IsOpen)
             {
                 SerialPort.DiscardInBuffer();//丢弃接收缓冲区的数据
                 return;
@@ -724,8 +724,6 @@ public class ModbusRtuModel : BindableBase
         }
     }
 
-
-
     public bool IsModbusCrcOk(List<byte> frame)
     {
         return IsModbusCrcOk(frame.ToArray());
@@ -752,7 +750,7 @@ public class ModbusRtuModel : BindableBase
         try
         {
             //若串口未开启则返回
-            if (!SerialPort.IsOpen)
+            if (SerialPort != null && !SerialPort.IsOpen)
             {
                 SerialPort.DiscardInBuffer();//丢弃接收缓冲区的数据
                 return;
