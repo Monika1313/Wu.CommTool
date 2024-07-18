d9948b6993d7214e59671e482a3c98b9821cc3a8
Monika <961501261@qq.com>
2024-07-18 18:15:41 +0800

    网卡配置设置改为ListBox

--- Modules/Wu.CommTool.Modules.NetworkTool/ViewModels/DesignViewModels/NetworkToolDesignViewModel.cs
+++ Modules/Wu.CommTool.Modules.NetworkTool/ViewModels/DesignViewModels/NetworkToolDesignViewModel.cs
@@ -7,5 +7,23 @@ public class NetworkToolDesignViewModel : NetworkToolViewModel
     public NetworkToolDesignViewModel()
     {
         获取物理网卡信息();
+
+        NetworkCardConfig n1 = new()
+        {
+            Ipv4s = [new Ipv4("192.168.1.3", "255.255.255.0"),
+                     new Ipv4("192.168.2.3", "255.255.255.0"),
+                     new Ipv4("192.168.3.3", "255.255.255.0"),
+                    ]
+        };
+        NetworkCardConfig n2 = new()
+        {
+            Ipv4s = [new Ipv4("192.168.1.3", "255.255.255.0"),
+                     new Ipv4("192.168.2.3", "255.255.255.0"),
+                     new Ipv4("192.168.3.3", "255.255.255.0"),
+                    ]
+        };
+        NetworkCardConfigs.Add(n1);
+        NetworkCardConfigs.Add(n2);
+        SelectedConfig = NetworkCardConfigs.First();
     }
 }
