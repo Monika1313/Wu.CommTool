ba7625fe4257cb6ddf2404ba536fe5b0f58f5767
weibin wu <961501261@qq.com>
2025-02-20 23:42:54 +0800

    💩ModbusRTU可定时发送消息

--- Modules/Wu.CommTool.Modules.ModbusRtu/Models/CustomFrame.cs
+++ Modules/Wu.CommTool.Modules.ModbusRtu/Models/CustomFrame.cs
@@ -2,13 +2,15 @@
 
 public partial class CustomFrame : ObservableObject
 {
+    private Task timerTask;
+
     public ModbusRtuModel Owner { get; private set; }
     public CustomFrame(string frame = "")
     {
         Frame = frame;
     }
 
-    public CustomFrame(ModbusRtuModel owner,string frame = "")
+    public CustomFrame(ModbusRtuModel owner, string frame = "")
     {
         Owner = owner;
         Frame = frame;
@@ -27,10 +29,55 @@ public partial class CustomFrame : ObservableObject
     bool enableTimer;
 
     /// <summary>
-    /// 发送间隔 单位毫秒
+    /// 发送间隔 单位毫秒 最小50ms
     /// </summary>
-    [ObservableProperty]
-    double interval = 1000;
+    public int Interval
+    {
+        get => interval;
+        set
+        {
+            if (value >= 50)
+            {
+                SetProperty(ref interval, value);
+            }
+            else
+            {
+                SetProperty(ref interval, 50);
+            }
+        }
+    }
+    private int interval = 1000;
 
+    /// <summary>
+    /// 启用定时发送
+    /// </summary>
+    public bool Enable
+    {
+        get => enable;
+        set
+        {
+            SetProperty(ref enable, value);
+            if (value)
+            {
+                //启动定时发送线程
+                timerTask = new Task(TimerTask);
+                timerTask.Start();
+            }
+        }
+    }
+    private bool enable;
 
+    private async void TimerTask()
+    {
+        while (Enable)
+        {
+            //若串口已打开则发送帧, 否则等待
+            if (Owner.ComConfig.IsOpened && !string.IsNullOrWhiteSpace(Frame))
+            {
+                //发送帧
+                Owner.SendCustomFrame(this);
+            }
+            await Task.Delay(Interval);
+        }
+    }
 }
